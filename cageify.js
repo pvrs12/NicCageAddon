7e3143574ed273e41aa1921aa61a380bf011d2f1
patrick vares <pvrs12@gmail.com>
2016-03-21 15:44:30 +0000

    working

--- cageify.js
+++ cageify.js
@@ -1,14 +1,33 @@
+var nicPrefix = "nic";
 var nicCount = 20;
 
 function getNic(){
-	int nicNum = Math.random()*nicCount;
-	return "images/"+nicNum+".jpg";
+	var nicNum = Math.floor(Math.random()*nicCount);
+	return "images/"+nicPrefix+nicNum+".jpg";
 }
 
-document.body.addEventListener("load",function(e){
-	var images = document.getElementByClass("img");
-	for(image in images){
-		image.src=getNic();
-		console.log(image.src);
-	}
+function replaceImages(){
+	Array.prototype.map.call(document.images,function(img){
+		//don't re-nic images
+		if(img.className.indexOf('nicced')>-1){
+			return;
+		}
+		img.classList.add('nicced');
+		//retain the original dimensions
+		var width = img.width;
+		var height = img.height;
+		img.style.width = width+'px';
+		img.style.height = height+'px';
+
+		var loc = chrome.extension.getURL(getNic());
+		img.src = loc;
+		if(img.srcset){
+			img.srcset = loc;
+		}
+	});
+}
+
+window.setTimeout(replaceImages,1000);
+document.body.addEventListener('click',function(){
+	replaceImages();
 });
